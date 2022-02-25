05d47f9fefab5fb496ff616b7018382990333e80
MJ <mj.zorick@wpengine.com>
2022-02-25 13:23:41 -0800

    added middleware

--- src/components/Cookie.tsx
+++ src/components/Cookie.tsx
@@ -17,6 +17,7 @@ function Cookie({
       className={styles.cookie}>
       <div className={styles.wrap}>
         <h3>{cookieInfo.cookieName}</h3>
+        {/* TODO - map this */}
         <pre>{JSON.stringify(cookieInfo.specialDiet, null, 2)}</pre>
         <p>Check out this {cookieInfo.cookieName}! </p>
         <img src={cookieImage.mediaItemUrl}></img>
